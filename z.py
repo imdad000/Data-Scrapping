3856b7582b2e96e5d36428226375fa1cb06a554b
Imdad Ali <imdadali@iiitdmj.ac.in>
2018-09-10 18:20:40 +0530

    Add files via upload

--- z.py
+++ z.py
@@ -3,4 +3,8 @@ import urllib.request
 source = urllib.request.urlopen('http://python.org/').read()
 soup = bs.BeautifulSoup(source,'lxml')
 print(soup.title)
-print(soup.find_all('p'))
+#for para in soup.find_all('p'):
+#	print(para.text)
+#print(soup.get_text())
+for url in soup.find_all('a'):
+    print(url.get('href'))
